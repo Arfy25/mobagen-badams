b985d5ba96caa19f67fbbd53018d2fb78218e7aa
Arfy25 <arfy25ben@gmail.com>
2026-09-01 13:36:51 -0400

    Test Commit

--- apps/flocking/behaviours/AlignmentRule.cpp
+++ apps/flocking/behaviours/AlignmentRule.cpp
@@ -6,6 +6,7 @@ glm::vec2 AlignmentRule::computeForce(const std::vector<BoidView>& neighborhood,
   // glm::vec2 can be divided by a float, which will divide each component of the vector by that float.
 
   // begin solution
+    // Test Commit Ben Adams
 
 
   return averageVelocity;
